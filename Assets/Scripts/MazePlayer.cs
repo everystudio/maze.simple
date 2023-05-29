2cd7f4eaee9e9bab8087a0d558174ff2e0a4cddf
watanabe@marida <watanabe.everystudio@gmail.com>
2023-05-29 19:46:23 +0900

    鍵とドアの処理を追加

--- Assets/Scripts/MazePlayer.cs
+++ Assets/Scripts/MazePlayer.cs
@@ -2,9 +2,30 @@
 
 public class MazePlayer : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public bool hasKey; // 持っている場合はTrue
+    private void Start()
     {
-        MazeManager.OnGoal.Invoke();
+        hasKey = false;
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Goal"))
+        {
+            MazeManager.OnGoal.Invoke();
+        }
+        else if (other.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            hasKey = true;
+        }
+        else if (other.CompareTag("Door"))
+        {
+            if (hasKey == true)
+            {
+                other.gameObject.SetActive(false);
+            }
+            Debug.Log(other.gameObject.name);
+        }
+    }
 }
